aa6dd2b6dd6a9a91d53b74391bb07db9e669b2d5
00kortex00 <koesuper228@gmail.com>
2026-01-01 23:37:48 +0200

    toast initialized

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { setToastDispatch } from "./hooks/useToast.ts";
+import { store } from "./store/store.ts";
 
+setToastDispatch(store.dispatch);
 createRoot(document.getElementById("root")!).render(<App />);
