b157e1b10927a6869ae65b117353d7b1b99d43a4
00kortex00 <77411838+00kortex00@users.noreply.github.com>
2026-01-16 11:00:27 +0200

    Update src/pages/SignUp/useSignUp.tsx
    Co-authored-by: Uladzislau Shautsou <70779585+retater56@users.noreply.github.com>

--- src/pages/SignUp/useSignUp.tsx
+++ src/pages/SignUp/useSignUp.tsx
@@ -29,12 +29,12 @@ export const useSignUp = () => {
   const [resendTimer, setResendTimer] = useState(0);
   const [isResending, setIsResending] = useState(false);
 
-  const signUpForm = useForm<RegisterFormData>({
+  const registerFormForm = useForm<RegisterFormData>({
     resolver: zodResolver(registerSchema),
     defaultValues: { email: "", password: "", confirmPassword: "" },
   });
 
-  const confirmForm = useForm<VerificationCodeFormData>({
+  const verificationCodeForm = useForm<VerificationCodeFormData>({
     resolver: zodResolver(verificationCodeSchema),
     defaultValues: { code: "" },
   });
