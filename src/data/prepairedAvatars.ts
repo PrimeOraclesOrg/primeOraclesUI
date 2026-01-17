2dd0ceee80a896b83c9399c11eb6dff06063a741
00kortex00 <koesuper228@gmail.com>
2026-01-17 21:22:31 +0200

    default avatars added to client

--- src/data/prepairedAvatars.ts
+++ src/data/prepairedAvatars.ts
@@ -1,10 +1,10 @@
 export const PREPAIRED_AVATARS = [
-  "https://ooglglelvfkjzfxrcsjg.supabase.co/storage/v1/object/public/default_avatars/avatar1.png",
-  "https://ooglglelvfkjzfxrcsjg.supabase.co/storage/v1/object/public/default_avatars/avatar2.png",
-  "https://ooglglelvfkjzfxrcsjg.supabase.co/storage/v1/object/public/default_avatars/avatar3.png",
-  "https://ooglglelvfkjzfxrcsjg.supabase.co/storage/v1/object/public/default_avatars/avatar4.png",
-  "https://ooglglelvfkjzfxrcsjg.supabase.co/storage/v1/object/public/default_avatars/avatar5.png",
-  "https://ooglglelvfkjzfxrcsjg.supabase.co/storage/v1/object/public/default_avatars/avatar6.png",
-  "https://ooglglelvfkjzfxrcsjg.supabase.co/storage/v1/object/public/default_avatars/avatar7.png",
-  "https://ooglglelvfkjzfxrcsjg.supabase.co/storage/v1/object/public/default_avatars/avatar8.png",
+  "/img/default_avatars/avatar1.png",
+  "/img/default_avatars/avatar2.png",
+  "/img/default_avatars/avatar3.png",
+  "/img/default_avatars/avatar4.png",
+  "/img/default_avatars/avatar5.png",
+  "/img/default_avatars/avatar6.png",
+  "/img/default_avatars/avatar7.png",
+  "/img/default_avatars/avatar8.png",
 ];
