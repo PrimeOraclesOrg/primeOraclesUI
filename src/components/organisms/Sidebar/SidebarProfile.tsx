1e0dbfe71daa45ebbe83b3e9494c576700e8cd01
00kortex00 <koesuper228@gmail.com>
2026-01-21 20:17:22 +0200

    sidebar profile

--- src/components/organisms/Sidebar/SidebarProfile.tsx
+++ src/components/organisms/Sidebar/SidebarProfile.tsx
@@ -1,28 +1,51 @@
 import { UserAvatar } from "@/components/molecules/UserAvatar/UserAvatar";
+import { Button } from "@/components/ui/button";
+import { selectAuthProfile, selectAuthUser } from "@/store";
 import { cn } from "@/utils";
-import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link, NavLink } from "react-router-dom";
 
 interface SidebarProfileProps {
   isActive: (href: string) => boolean;
 }
 
 export const SidebarProfile = ({ isActive }: SidebarProfileProps) => {
+  const profile = useSelector(selectAuthProfile);
+
   return (
     <div className="p-4 border-t border-sidebar-border">
-      <NavLink
-        to="/profile"
-        className={cn(
-          "flex items-center gap-3 px-3 py-2.5 rounded-lg transition-colors",
-          isActive("/profile")
-            ? "text-foreground bg-sidebar-accent"
-            : "text-sidebar-foreground hover:text-foreground hover:bg-sidebar-accent/50"
-        )}
-      >
-        <div className="w-8 h-8 rounded-full bg-muted flex items-center justify-center">
-          <UserAvatar />
-        </div>
-        <span className="text-sm font-medium">Личный профиль</span>
-      </NavLink>
+      {profile && (
+        <NavLink
+          to="/profile"
+          className={cn(
+            "flex items-center gap-3 px-3 py-2.5 rounded-lg transition-colors group/profile",
+            isActive("/profile")
+              ? "text-foreground bg-sidebar-accent"
+              : "text-sidebar-foreground hover:text-foreground hover:bg-sidebar-accent/50"
+          )}
+        >
+          <div className="w-8 h-8 rounded-full bg-muted flex items-center justify-center">
+            <UserAvatar />
+          </div>
+          <div className="flex flex-col min-w-0">
+            <span className="text-xs font-medium text-nowrap text-ellipsis overflow-hidden">
+              Личный профиль
+            </span>
+            <span className="text-md font-medium text-foreground text-nowrap text-ellipsis overflow-hidden group-hover/profile:text-primary">
+              {profile?.name}
+            </span>
+            <span className="text-xs font-medium text-nowrap text-ellipsis overflow-hidden">
+              @{profile?.username}
+            </span>
+          </div>
+        </NavLink>
+      )}
+
+      {!profile && (
+        <Button className="w-full" asChild>
+          <Link to={"/login"}>Присоеденится</Link>
+        </Button>
+      )}
     </div>
   );
 };
