52cfb145e6cae454ccf635a173c4b03e326deb3a
00kortex00 <koesuper228@gmail.com>
2026-01-22 11:53:45 +0200

    footer, sticky sidebar

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Sidebar, MobileHeader } from "@/components/organisms";
+import { Sidebar, MobileHeader, Footer } from "@/components/organisms";
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -8,11 +8,14 @@ interface MainLayoutProps {
 export function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="min-h-screen bg-background">
-      <div className="hidden lg:block">
-        <Sidebar />
-      </div>
       <MobileHeader />
-      <main className="lg:ml-64 min-h-screen pt-[57px] lg:pt-0">{children}</main>
+      <div className="flex">
+        <div className="hidden lg:block">
+          <Sidebar />
+        </div>
+        <main className="min-h-screen pt-[57px] lg:pt-0 min-w-0">{children}</main>
+      </div>
+      <Footer />
     </div>
   );
 }
