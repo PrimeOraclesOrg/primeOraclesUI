f52bb9f85fb2269f15a5c8d0664c6ae73ed1e100
00kortex00 <koesuper228@gmail.com>
2026-02-09 10:52:47 +0200

    removed wrapperClassName prop from MainLayout, added styles directly in MainLayout

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -1,13 +1,11 @@
 import { ReactNode } from "react";
 import { Sidebar, MobileHeader, Footer } from "@/components/organisms";
-import { cn } from "@/utils";
 
 interface MainLayoutProps {
   children: ReactNode;
-  wrapperClassName?: string;
 }
 
-export function MainLayout({ children, wrapperClassName }: MainLayoutProps) {
+export function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="min-h-screen bg-background ">
       <MobileHeader />
@@ -16,9 +14,7 @@ export function MainLayout({ children, wrapperClassName }: MainLayoutProps) {
           <Sidebar />
         </div>
         <main className="w-full min-h-screen pt-[57px] lg:pt-0 min-w-0">
-          <div className={cn("m-4 md:border md:rounded-lg border-border", wrapperClassName)}>
-            {children}
-          </div>
+          <div className="m-4 md:border md:rounded-lg border-border mx-0 md:mx-4">{children}</div>
         </main>
       </div>
       <Footer />
