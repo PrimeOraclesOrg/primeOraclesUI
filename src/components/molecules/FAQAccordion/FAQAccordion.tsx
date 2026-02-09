5085b2473201deb3e89096c8d2e5ba7afb2d269a
00kortex00 <koesuper228@gmail.com>
2026-02-09 10:43:45 +0200

    removed border from last FAQ item

--- src/components/molecules/FAQAccordion/FAQAccordion.tsx
+++ src/components/molecules/FAQAccordion/FAQAccordion.tsx
@@ -5,6 +5,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { FAQ } from "@/types";
+import { cn } from "@/utils";
 import { FC } from "react";
 
 interface FAQAccordionProps {
@@ -18,8 +19,12 @@ export const FAQAccordion: FC<FAQAccordionProps> = ({ questions }) => {
       collapsible
       className="max-w-3xl w-full mx-auto lg:text-xl text-lg border border-border px-12 py-2 rounded-2xl bg-[#12121280]"
     >
-      {questions.map((faq) => (
-        <AccordionItem key={faq.position} value={faq.position.toString()} className="border-border">
+      {questions.map((faq, index) => (
+        <AccordionItem
+          key={faq.position}
+          value={faq.position.toString()}
+          className={cn("border-border", index === questions.length - 1 && "border-none")}
+        >
           <AccordionTrigger className="text-foreground hover:text-primary text-start">
             {faq.question}
           </AccordionTrigger>
